419a0452fbc315b3d27d4b70d91dc9bd5247e439
evaneddleston <eddleston.e@northeastern.edu>
2022-02-10 13:27:58 -0500

    I tried my best but can't figure this assignment out right now.

--- main.js
+++ main.js
@@ -12,3 +12,10 @@ Modified: 12/06/21
 function button1Clicked() {
     console.log("Button 1 was clicked"); 
 }
+// we need to select the button-div (using its id) 
+    let buttonDiv = document.getElementById("button-div"); 
+    // then we buttonDiv's content
+    buttonDiv.innerHTML = newText; 
+
+function button2Clicked() {
+    console.log("Button 2 was clicked"); 
